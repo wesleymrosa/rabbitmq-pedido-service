b9bf72888f6a403ec1967c01c6a0e7caf5319c70
wesleymrosa <wesleymrosa@gmail.com>
2025-05-21 08:53:45 -0300

    Criado projeto inicial rabbitmq-pedido-service com estrutura base gerada via Spring Initializr

---
+++ src/main/java/br/wesley/rabbitmq/pedido/RabbitmqPedidoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.wesley.rabbitmq.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPedidoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPedidoServiceApplication.class, args);
+	}
+
+}
